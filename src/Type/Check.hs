6b8574265c5ed3486ad4b238bc6614b250d34e1f
yunjuanhuakai <makaiyk@163.com>
2018-07-30 21:56:31 +0000

    check end

--- src/Type/Check.hs
+++ src/Type/Check.hs
@@ -4,7 +4,7 @@
 module Type.Check where
 
 import           Ast
--- import           Helper
+import Type.State
 import qualified Data.Map.Strict               as Map
 import           Control.Monad.Trans.State.Strict
                                                 ( State(..)
@@ -14,31 +14,31 @@ import           Control.Monad.Trans.State.Strict
                                                 )
 import           Control.Monad
 
+---------------------- check decl -------------------------------
+
 ----------------------- rcursive ----------------------------------
 
-data Flag = Checking | Checked
-type CheckState = Map.Map Type Flag
-
-checkRcursiveParams :: Type -> [Type] -> State CheckState Bool
+checkRcursiveParams :: Type -> [Type] -> CbCheck Bool
 checkRcursiveParams t ts = do
-    s <- get
+    cst <- get
+    let s = rcursive cst
     case Map.lookup t s of
         Just Checking -> pure False
         Just Checked  -> pure True
         Nothing       -> do
-            put $ Map.insert t Checking s
-            res <- foldM
-                (\b t -> do
-                    res <- checkRcursive t
-                    pure $ b && res
-                )
-                True
-                ts
-            s <- get
-            put $ Map.insert t Checked s
-            pure res
+          put $ cst { rcursive = Map.insert t Checking s }
+          res <- foldM
+              (\b t -> do
+                  res <- checkRcursive t
+                  pure $ b && res
+              )
+              True
+              ts
+          cst <- get
+          put $ cst { rcursive = Map.insert t Checking $ rcursive cst }
+          pure res
 
-checkRcursive :: Type -> State CheckState Bool
+checkRcursive :: Type -> CbCheck Bool
 checkRcursive t@(CbStruct name params) =
     checkRcursiveParams t (fmap paramType params)
 checkRcursive t@(CbUnion name params) =
@@ -46,81 +46,81 @@ checkRcursive t@(CbUnion name params) =
 checkRcursive (CbArray t _) = checkRcursive t
 checkRcursive (CbConst t  ) = checkRcursive t
 checkRcursive t             = do
-    s <- get
-    put $ Map.insert t Checked s
+    cst <- get
+    put $ cst { rcursive = Map.insert t Checking $ rcursive cst }
     pure True
 
 ------------------------ check expr ----------------------------------
 
-checkTypeBinByArithmetic :: Type -> Type -> Either String Type
+checkTypeBinByArithmetic :: Type -> Type -> CbCheck Type
 checkTypeBinByArithmetic lt rt = if
-  | isBool lt || isBool rt -> Left "无法对bool类型运用这个运算"
+  | isBool lt || isBool rt -> fail "无法对bool类型运用这个运算"
   | otherwise -> case typeTable !! typeIndex lt !! typeIndex rt of
-    Just t  -> Right t
-    Nothing -> Left "无法提升的类型"
+    Just t  -> pure t
+    Nothing -> fail "无法提升的类型"
 
-checkTypeBinByPOrE :: Type -> Type -> Either String Type
+checkTypeBinByPOrE :: Type -> Type -> CbCheck Type
 checkTypeBinByPOrE lt rt = if
-  | isPtr lt && isPtr rt -> Left "两指针之间无法运行该运算"
-  | isPtr lt -> if isInteger rt then Right lt else Left "对指针类型加减必须为整形"
-  | isPtr rt -> if isInteger lt then Right rt else Left "对指针类型加减必须为整形"
+  | isPtr lt && isPtr rt -> fail "两指针之间无法运行该运算"
+  | isPtr lt -> if isInteger rt then pure lt else fail "对指针类型加减必须为整形"
+  | isPtr rt -> if isInteger lt then pure rt else fail "对指针类型加减必须为整形"
   | otherwise -> checkTypeBinByArithmetic lt rt
 
-checkTypeBin :: Type -> String -> Type -> Either String Type
+checkTypeBin :: Type -> String -> Type -> CbCheck Type
 checkTypeBin lt op rt = if
   | op `elem` ["+", "-"] -> checkTypeBinByPOrE lt rt
   | op `elem` ["*", "/"] -> checkTypeBinByArithmetic lt rt
   | op `elem` ["<", "<=", ">", ">="] -> const CbBool <$> checkTypeBinByArithmetic lt rt
   | op `elem` ["%", "&", "|", "^", ">>", "<<"] ->  if
     | isInteger lt && isInteger rt -> checkTypeBinByArithmetic lt rt
-    | otherwise -> Left "无法对非整形数字运用这个运算符"
+    | otherwise -> fail "无法对非整形数字运用这个运算符"
   | op `elem` ["==", "!="] -> case typeTable !! typeIndex lt !! typeIndex rt of
-    Just t  -> Right t
-    Nothing -> Left "无法提升的类型"
+    Just t  -> pure t
+    Nothing -> fail "无法提升的类型"
   | op `elem` ["&&", "||"] -> if
-    | isBool lt && isBool rt -> Right CbBool
-    | otherwise -> Left "逻辑运算符两侧须为bool类型"
-  | otherwise -> Left "未知的操作符"
+    | isBool lt && isBool rt -> pure CbBool
+    | otherwise -> fail "逻辑运算符两侧须为bool类型"
+  | otherwise -> fail "未知的操作符"
 
-checkTypeUnary :: String -> Type -> Either String Type
+checkTypeUnary :: String -> Type -> CbCheck Type
 checkTypeUnary op t = if
     | op `elem` ["+", "-"] -> if isSigned t
         then pure t
-        else Left "无法对非有符号数字调用该操作符"
-    | op == "~" -> if isInteger t then pure t else Left "无法对非整数类型调用该操作符"
-    | op == "!" -> if isBool t then pure t else Left "无法对非布尔类型调用该操作符"
+        else fail "无法对非有符号数字调用该操作符"
+    | op == "~" -> if isInteger t then pure t else fail "无法对非整数类型调用该操作符"
+    | op == "!" -> if isBool t then pure t else fail "无法对非布尔类型调用该操作符"
     | op `elem` ["++", "--"] -> if isNumber t || isPtr t
         then pure t
-        else Left "仅可对指针或数字类型调用该操作符"
-    | otherwise -> Left "未知的操作符"
+        else fail "仅可对指针或数字类型调用该操作符"
+    | otherwise -> fail "未知的操作符"
 
-checkTypeAssign :: Type -> Type -> Either String Type
-checkTypeAssign (CbConst _) _  = Left "对 const 类型的表达式赋值"
+checkTypeAssign :: Type -> Type -> CbCheck Type
+checkTypeAssign (CbConst _) _  = fail "对 const 类型的表达式赋值"
 checkTypeAssign lt          rt = pormot' rt lt
 
 checkCall :: Type -> [Type] -> Bool
 checkCall (CbFunction fc pats) params = all (uncurry pormot) (zip params pats)
 
-pormot' :: Type -> Type -> Either String Type
+pormot' :: Type -> Type -> CbCheck Type
 pormot' source target =
-  if pormot source target then pure target else Left "类型不匹配"
+  if pormot source target then pure target else fail "类型不匹配"
 
-maybeToEither :: Maybe a -> Either String a
+maybeToEither :: Maybe a -> CbCheck a
 maybeToEither (Just a) = pure a
-maybeToEither Nothing  = Left "类型不匹配"
+maybeToEither Nothing  = fail "类型不匹配"
 
-pormotTwo :: Type -> Type -> Either String Type
+pormotTwo :: Type -> Type -> CbCheck Type
 pormotTwo t1 t2 = maybeToEither $ typeTable !! typeIndex t1 !! typeIndex t2
 
 -- 计算并检测表达式类型
-computeType :: Expr -> Either String Type
+computeType :: Expr -> CbCheck Type
 computeType (Funcall fc params fun) = do
   funType    <- computeType fun
   paramTypes <- mapM computeType params
   case funType of
     t@(CbFunction rt pats) ->
-      if checkCall t paramTypes then pure rt else Left "函数调用参数类型不匹配"
-    _ -> Left "对非函数表达式进行调用"
+      if checkCall t paramTypes then pure rt else fail "函数调用参数类型不匹配"
+    _ -> fail "对非函数表达式进行调用"
 computeType (SizeofType fc t ) = pure CbLong
 computeType (SizeofExpr fc e_) = pure CbLong
 computeType (Assign fc lv rv ) = if isLValue lv
@@ -128,13 +128,13 @@ computeType (Assign fc lv rv ) = if isLValue lv
     lt <- computeType lv
     rt <- computeType rv
     checkTypeAssign lt rt
-  else Left "对非左值做赋值操作"
+  else fail "对非左值做赋值操作"
 computeType (OpAssign fc op lv rv) = if isLValue lv
   then do
     rt <- computeType $ Binary NoFC op lv rv
     lt <- computeType lv
     checkTypeAssign lt rt
-  else Left "对非左值做赋值操作"
+  else fail "对非左值做赋值操作"
 computeType (Binary fc op l r) = do
   lt <- computeType l
   rt <- computeType r
@@ -149,36 +149,36 @@ computeType (Cond fc cond then_ else_) = do
   elseType <- computeType else_
   if isBool condType
     then pormotTwo thenType elseType
-    else Left "作为条件的表达式并非bool类型"
+    else fail "作为条件的表达式并非bool类型"
 computeType (Address fc expr) = if isLValue expr
   then do
     t <- computeType expr
     case t of
       CbFunction{} -> pure t -- 函数类型会进行隐式指针类型转换
       _            -> pure $ CbPtr t
-  else Left "对非左值做取址操作"
+  else fail "对非左值做取址操作"
 computeType (Dereference fc expr) = do
   t <- computeType expr
   case t of
     CbFunction{} -> pure t
     CbPtr t      -> pure t
-    _            -> Left "对非指针对象解引用"
+    _            -> fail "对非指针对象解引用"
 computeType (Member fc name expr) = do
   t <- computeType expr
   case findMem t name of
     Just (Param t _ _) -> pure t
-    Nothing            -> Left $ "无法获取 " ++ name ++ " 成员的类型"
+    Nothing            -> fail $ "无法获取 " ++ name ++ " 成员的类型"
 computeType (PtrMember fc name expr) = do
   t <- computeType expr
   case findPtrMem t name of
     Just (Param t _ _) -> pure t
-    Nothing            -> Left $ "无法获取 " ++ name ++ " 成员的类型"
+    Nothing            -> fail $ "无法获取 " ++ name ++ " 成员的类型"
 computeType (Arrayref fc ve ie) = do
   vt <- computeType ve
   it <- computeType ie
   if isPtr vt && isInteger it
     then maybeToEither $ mem vt
-    else Left "对非指针类型的值解引用"
+    else fail "对非指针类型的值解引用"
 computeType (Varable fc name type_) = pure type_
 computeType IntLiteral{}            = pure $ CbConst CbInt
 computeType FloatLiteral{}          = pure $ CbConst CbFloat
--- src/Type/Resolver.hs
+++ src/Type/Resolver.hs
@@ -8,33 +8,53 @@ import           Ast
 import           Helper
 import           IState
 import           Type.Check
+import           Type.State
+import           Data.Maybe
+import           Data.Functor
 import           Control.Monad.Trans.State.Strict
                                                 ( get
                                                 , put
                                                 , evalState
+                                                , StateT(..)
                                                 )
 
-checkRcursive' :: Declaration -> Cb Declaration
+checkRcursive' :: Declaration -> CbCheck Declaration
 checkRcursive' decl = do
-  let res = evalState (checkRcursive $ declToType decl) Map.empty
+  res <- checkRcursive $ declToType decl
   if res then pure decl else fail "存在循环定义的结构体"
 
-unit :: Unit -> Cb Unit
+unit :: Unit -> CbCheck Unit
 unit (Unit imps decls) = Unit imps <$> mapM decl decls
 
-param :: Param -> Cb Param
+param :: Param -> CbCheck Param
 param (Param t name init) =
-  Param <$> type' t <*> pure name <*> mapM exprAndCheck' init
+  Param <$> type' t <*> pure name <*> mapM exprAndCheck init
 
-decl :: Declaration -> Cb Declaration
+checkVar :: Declaration -> CbCheck Declaration
+checkVar d@(Variable fc t name init) = if isJust init
+  then do
+    vt <- fmap (\s -> exprTypes s Map.! fromJust init) get
+    if pormot vt t then pure d else fail "初始化语句与变量类型不符"
+  else pure d
+
+checkReturn :: Type -> [Stmt] -> CbCheck ()
+checkReturn CbVoid []                  = pure ()
+checkReturn t      []                  = fail "没有找到必要的返回值类型"
+checkReturn t      [Return _ Nothing ] = pure ()
+checkReturn t      [Return _ (Just e)] = computeType e
+  >>= \vt -> if pormot vt t then pure () else fail "函数return语句的类型与返回值不符"
+checkReturn t (stmt : stmts) = checkReturn t stmts
+
+checkFunction :: Declaration -> CbCheck Declaration
+checkFunction d@(Function fc rt name params body) = case body of
+  (Block _ _ stmts) -> checkReturn rt stmts $> d
+  _                 -> fail "函数定义的结构异常"
+
+decl :: Declaration -> CbCheck Declaration
 decl (Variable fc t name init) =
-  Variable fc <$> type' t <*> pure name <*> mapM exprAndCheck' init
+  Variable fc <$> type' t <*> pure name <*> mapM exprAndCheck init >>= checkVar
 decl (Function fc rt name params body) =
-  Function fc
-    <$> type' rt
-    <*> pure name
-    <*> mapM param params
-    <*> stmt body
+  Function fc <$> type' rt <*> pure name <*> mapM param params <*> stmt body
 decl (UndefineFunction fc rt name params) =
   UndefineFunction fc <$> type' rt <*> pure name <*> mapM param params
 decl (Struct fc name params) =
@@ -43,79 +63,66 @@ decl (Union fc name params) =
   Union fc name <$> mapM param params >>= checkRcursive'
 decl (Typedef fc t name) = Typedef fc <$> type' t <*> pure name
 
-checkCond :: Expr -> Cb Expr
+checkCond :: Expr -> CbCheck Expr
 checkCond e = do
-  (e, t) <- exprAndCheck e
-  if isBool t
-    then pure e
-    else fail "作为条件的表达式不是bool类型的"
+  t <- fmap (\s -> exprTypes s Map.! e) get
+  if isBool t then pure e else fail "作为条件的表达式不是bool类型的"
 
-checkCase :: Expr -> Cb Expr
+checkCase :: Expr -> CbCheck Expr
 checkCase e = do
-  (e, t) <- exprAndCheck e
-  if pormot t CbULong 
-    then pure e
-    else fail "作为 case 语句的匹配值不符合为整数的限制"
+  t <- fmap (\s -> exprTypes s Map.! e) get
+  if pormot t CbULong then pure e else fail "作为 case 语句的匹配值不符合为整数的限制"
 
-stmt :: Stmt -> Cb Stmt
-stmt (If fc expr then_ else_) = If fc <$> checkCond expr <*> stmt then_ <*> mapM stmt else_
+stmt :: Stmt -> CbCheck Stmt
+stmt (If fc expr then_ else_) =
+  If fc <$> checkCond expr <*> stmt then_ <*> mapM stmt else_
 stmt (Switch fc expr stmts) =
-  Switch fc <$> exprAndCheck' expr <*> mapM stmt stmts
+  Switch fc <$> exprAndCheck expr <*> mapM stmt stmts
 stmt (Case fc expr body) = Case fc <$> checkCase expr <*> stmt body
 stmt (For fc init cond next body) =
-  For fc
-    <$> stmt init
-    <*> mapM checkCond cond
-    <*> stmt next
-    <*> stmt body
-stmt (While fc cond body) =
-  While fc <$> checkCond cond <*> stmt body
-stmt (DoWhile fc body cond) =
-  DoWhile fc <$> stmt body <*> checkCond cond
-stmt (Block fc decls stmts) =
-  Block fc <$> mapM decl decls <*> mapM stmt stmts
-stmt (Return     fc expr) = Return fc <$> mapM exprAndCheck' expr
-stmt (Expression fc expr) = Expression fc <$> exprAndCheck' expr
-stmt s                    = pure s
+  For fc <$> stmt init <*> mapM checkCond cond <*> stmt next <*> stmt body
+stmt (While   fc cond  body ) = While fc <$> checkCond cond <*> stmt body
+stmt (DoWhile fc body  cond ) = DoWhile fc <$> stmt body <*> checkCond cond
+stmt (Block   fc decls stmts) = Block fc <$> mapM decl decls <*> mapM stmt stmts
+stmt (Return     fc expr    ) = Return fc <$> mapM exprAndCheck expr
+stmt (Expression fc expr    ) = Expression fc <$> exprAndCheck expr
+stmt s                        = pure s
 
-exprAndCheck :: Expr -> Cb (Expr, Type)
+exprAndCheck :: Expr -> CbCheck Expr
 exprAndCheck expr = do
-  ist <- get
-  e   <- expr' expr
-  case computeType e of
-    Right t   -> pure (e, t)
-    Left  err -> fail err
+  res <- expr' expr
+  t   <- computeType res
+  cst <- get
+  put $ cst { exprTypes = Map.insert res t $ exprTypes cst }
+  pure res
 
-exprAndCheck' :: Expr -> Cb Expr
-exprAndCheck' expr = fst <$> exprAndCheck expr
-
-expr' :: Expr -> Cb Expr
+expr' :: Expr -> CbCheck Expr
 expr' (Funcall fc params fun) =
-  Funcall fc <$> mapM exprAndCheck' params <*> exprAndCheck' fun
+  Funcall fc <$> mapM exprAndCheck params <*> exprAndCheck fun
 expr' (SizeofType fc t) = SizeofType fc <$> type' t
-expr' (SizeofExpr fc e) = SizeofExpr fc <$> exprAndCheck' e
-expr' (Assign fc lv rv) = Assign fc <$> exprAndCheck' lv <*> exprAndCheck' rv
+expr' (SizeofExpr fc e) = SizeofExpr fc <$> exprAndCheck e
+expr' (Assign fc lv rv) = Assign fc <$> exprAndCheck lv <*> exprAndCheck rv
 expr' (OpAssign fc s lv rv) =
-  OpAssign fc s <$> exprAndCheck' lv <*> exprAndCheck' rv
-expr' (Binary fc op l r ) = Binary fc op <$> exprAndCheck' l <*> exprAndCheck' r
-expr' (Unary  fc op expr) = Unary fc op <$> exprAndCheck' expr
-expr' (Prefix fc op expr) = Prefix fc op <$> exprAndCheck' expr
-expr' (Suffix fc op expr) = Suffix fc op <$> exprAndCheck' expr
+  OpAssign fc s <$> exprAndCheck lv <*> exprAndCheck rv
+expr' (Binary fc op l r ) = Binary fc op <$> exprAndCheck l <*> exprAndCheck r
+expr' (Unary  fc op expr) = Unary fc op <$> exprAndCheck expr
+expr' (Prefix fc op expr) = Prefix fc op <$> exprAndCheck expr
+expr' (Suffix fc op expr) = Suffix fc op <$> exprAndCheck expr
 expr' (Cast   fc t  e   ) = Cast fc <$> type' t <*> pure e
 expr' (Cond fc cond then_ else_) =
-  Cond fc <$> exprAndCheck' cond <*> exprAndCheck' then_ <*> exprAndCheck' else_
-expr' (Address     fc expr   ) = Address fc <$> exprAndCheck' expr
-expr' (Dereference fc expr   ) = Dereference fc <$> exprAndCheck' expr
-expr' (Member    fc name expr) = Member fc name <$> exprAndCheck' expr
-expr' (PtrMember fc name expr) = PtrMember fc name <$> exprAndCheck' expr
-expr' (Arrayref fc ve ie) = Arrayref fc <$> exprAndCheck' ve <*> exprAndCheck' ie
+  Cond fc <$> exprAndCheck cond <*> exprAndCheck then_ <*> exprAndCheck else_
+expr' (Address     fc expr   ) = Address fc <$> exprAndCheck expr
+expr' (Dereference fc expr   ) = Dereference fc <$> exprAndCheck expr
+expr' (Member    fc name expr) = Member fc name <$> exprAndCheck expr
+expr' (PtrMember fc name expr) = PtrMember fc name <$> exprAndCheck expr
+expr' (Arrayref fc ve ie) = Arrayref fc <$> exprAndCheck ve <*> exprAndCheck ie
 expr' (Varable   fc name t   ) = Varable fc name <$> type' t
 expr' e                        = pure e
 
-type' :: Type -> Cb Type
+type' :: Type -> CbCheck Type
 type' (CbUnknown name) = do
-  ist <- get
-  case Map.lookup name (types ist) of
+  cst <- get
+  case Map.lookup name (types $ ist cst) of
     Just t  -> pure t
     Nothing -> fail $ "未知的类型名" ++ name
 type' t = pure t
